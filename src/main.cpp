ca543eb1e113de1da9f9b646d193f88bc4715461
bigoren <bigoreo@gmail.com>
2019-03-13 12:23:25 +0200

    Adding fastled and OTA into the mix, cpu frequency uppped to 160MHz

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #define ARDUINOJSON_USE_LONG_LONG 1
-//#include <NTP.h>
+#define FASTLED_ESP8266_DMA
+
+#include "FastLED.h"
 #include <ESP8266WiFi.h>
+// #include <ESP8266mDNS.h>
 #include <WebSocketsClient.h>
 #include <ArduinoJson.h>
+#include <ArduinoOTA.h>
 #include "timesync.hpp"
+
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define DATA_PIN    9
+//#define CLK_PIN   4
+#define LED_TYPE    WS2811
+#define COLOR_ORDER RGB
+#define NUM_LEDS    50
+#define BRIGHTNESS  100
 
 /*
 We don't want to have private ssid and passwords pushed to git.
@@ -23,6 +38,9 @@ and add the following text to it:
 #endif
 */
 #include "credentials.h"
+
+CRGB leds[NUM_LEDS];
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 int ledState = LOW;             // ledState used to set the LED
 
@@ -113,10 +131,22 @@ void printTimeUTC (unsigned long timeFromEpochSec, unsigned long timeFromEpochMi
 }
 
 void setup() {
+  // Start serial output, do not start after FastLED init!
+  Serial.begin(115200);
+  delay(100);
+  Serial.setDebugOutput(true);
+
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS)
+         .setCorrection(TypicalLEDStrip)
+         .setDither(BRIGHTNESS < 255);
+
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);
+
   pinMode(2, OUTPUT);
 
-  Serial.begin(115200);
-  Serial.println();
+
   startWiFi();
 
   Serial.println("WiFi connected");
@@ -125,7 +155,7 @@ void setup() {
 
   Serial.println("Connected to wifi");
 
-  IPAddress songServerIP(10,0,0,37);
+  IPAddress songServerIP(10,0,0,102);
   IPAddress timeServerIP(10,0,0,102);
   // IPAddress timeServerIP;
   // WiFi.hostByName("WhereNowPi.local", timeServerIP);
@@ -149,10 +179,52 @@ void setup() {
   // expect pong from server within 3000 ms
   // consider connection disconnected if pong is not received 2 times
   webSocket.enableHeartbeat(15000, 3000, 2);
+
+
+  /////// ADDED SECTION FOR OTA ///////////
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname(host);
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA setup sequence finished");
 }
 
 void loop() {
-
+  ArduinoOTA.handle();
   timeSync.loop();
 
   if(!timeSync.m_isTimeValid) {
@@ -165,22 +237,37 @@ void loop() {
     int32_t songStartTime = (int32_t)((int64_t)startTimeFromPlayer - espStartTime); // can this be int32_t??
     unsigned long currentTim = millis();
     uint32_t songCurrentTime = currentTim - songStartTime;
-    if ((((songCurrentTime/1000)%10 == 0) && (songCurrentTime%1000 < 500))) {
+		uint32_t songCurrentTimeMillis = songCurrentTime%1000;
+    uint32_t songCurrentTimeSecs = songCurrentTime/1000;
+    if (((songCurrentTimeSecs%10 == 0) && (songCurrentTimeMillis < 500))) {
       digitalWrite(2,HIGH);
-      Serial.print("songStartTime: "); Serial.println(songStartTime);
-      Serial.print("currentTim: "); Serial.println(currentTim);
-      Serial.print(songCurrentTime/1000);Serial.print(".");Serial.println(songCurrentTime%1000);
+      // Serial.print("songStartTime: "); Serial.println(songStartTime);
+      // Serial.print("currentTim: "); Serial.println(currentTim);
+      // Serial.print(songCurrentTimeSecs);Serial.print(".");Serial.println(songCurrentTimeMillis);
+			gHue = 0;
     }
-    else if (songCurrentTime%1000 < 100) {
+    else if (songCurrentTimeMillis < 100) {
       digitalWrite(2,HIGH);
-      Serial.print("songStartTime: "); Serial.println(songStartTime);
-      Serial.print("currentTim: "); Serial.println(currentTim);
-      Serial.print(songCurrentTime/1000);Serial.print(".");Serial.println(songCurrentTime%1000);
+      // Serial.print("songStartTime: "); Serial.println(songStartTime);
+      // Serial.print("currentTim: "); Serial.println(currentTim);
+      // Serial.print(songCurrentTimeSecs);Serial.print(".");Serial.println(songCurrentTimeMillis);
+			gHue = 0;
     }
     else {
       digitalWrite(2,LOW);
+			leds[0] = CRGB::Red;
+			float millisFloat = (float)(songCurrentTimeMillis - 100) / 900.0;
+			gHue = (uint8_t)(millisFloat * 255);
+			// Serial.println(gHue);
     }
   }
+
+	// FastLED's built-in rainbow generator
+  // fill_rainbow( leds, NUM_LEDS, gHue, 255 / NUM_LEDS);
+  fill_solid(leds, NUM_LEDS, CHSV(gHue, 255, 255));
+  FastLED.show();
+  // delay(10);
+  // gHue++;
 
   // unsigned long currentTime = millis();
   // unsigned long currentTimeSec = (currentTime / 1000) + timeSync.m_startTimeSec;
